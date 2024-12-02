7942b0ea79cec01c88743784f9fb0ad4a026d84e
Erick Estrada <erickestradaa1@hotmail.com>
2024-12-01 20:47:04 -0800

    Se sube Sesion 8

---
+++ Sesion_8.py
@@ -0,0 +1,33 @@
+
+emojis =  {"feliz": "😃"
+           , "amo": "😍"
+           , "risa": "🤣"
+           , "sonrisa": "😊"
+           , "llorar": "😭"
+           , "beso": "😘"
+           , "aplauso": "👏"
+           , "reir": "😁"
+           , "fuego": "🔥"
+           , "roto": "💔"x
+           , "pensando": "🤔"
+           , "maravillado": "🤩"
+           , "aburrido": "🙄"
+           , "güiño": "😉"
+           , "ok": "👌"
+           , "abrazo": "🤗"
+           , "cool": "😎"
+           , "enojado": "😠"
+           , "python": "🐍"}
+
+frase = input('Ingresa una frase por favor. ')
+
+palabras = frase.split(" ")
+
+respuesta = ""
+for i in palabras:
+    if i in emojis:
+        respuesta = respuesta+" "+emojis[i]
+    else:
+        respuesta = respuesta+" "+i
+
+print(respuesta)
